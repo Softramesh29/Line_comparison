8d7c3e012422b95c10b992cdc38090fb16ad6a3e
softramesh29 <softramesh29@gmail.com>
2022-04-14 11:55:42 +0530

    Calculating lenth of a line

---
+++ Calculatinglength.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.Linecomparison;
+
+public class Calculatinglength {
+	
+	public static void main(String [] args) {
+		int x1, x2, y1, y2;
+		double distance;
+		
+		x1 = 1;
+		y1 = 1;
+		x2 = 4;
+		y2 = 4;
+		
+		distance = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+		System.out.println("Distance is : " +distance);
+				
+	}
+}
